344a049a099e5410cc2fb687cc6e6ebf6fcf9efb
CG <greinke@novent.de>
2016-03-14 23:07:26 +0100

    Feat: Cache matrix computation result with inverse helper function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Specific functions to cache the inverse of a matrix 
+## by caching the first inverse computation
+##
+## Comment on implementation difference between provided sample.
+## It makes no sense to make inner working of makeCacheMatrix depended
+## of specific call order in cacheSolve. The actual interface of  
+## makeCacheMatrix should hide that and limit the interface to the 
+## actual concern it solves
 
-## Write a short comment describing this function
+
+## Function sets a variable given in the constructor and caches the result of a function call
+## after first call to cacheCall the function will always return the first result independent 
+## of further cacheCall calls with different fun types
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
+    }
+    get <- function() x
+    cacheCall <- function(fun,...){
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        m <<- fun(x,...)
+        m
+    } 
+    list(set = set, get = get,
+            cacheCall = cacheCall)
 }
 
 
-## Write a short comment describing this function
+## using the solve function to cache the result 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        x$cacheCall(solve,...)
 }
